6bb4f947e9bcd394b4dff244928b54758882ae6f
Ubuntu <sahirkeshwani@yahoo.com>
2013-07-12 07:30:04 +0000

    Added index.html printing

--- web.js
+++ web.js
@@ -1,12 +1,6 @@
-var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var data = fs.readFileSync('index.html')
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+console.log(data.toString('utf-8'));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
