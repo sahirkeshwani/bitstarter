7eb633efad740115001d014f7de78433c417cdfb
Ubuntu <sahirkeshwani@yahoo.com>
2013-07-13 08:33:27 +0000

    Added web.js for try3

--- web.js
+++ web.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
 var data = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(data.toString('utf-8'));
 });
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 
