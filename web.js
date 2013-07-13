2e993da410de594c6f2ec86d796189a2dc4323c8
Ubuntu <sahirkeshwani@yahoo.com>
2013-07-13 07:48:50 +0000

    Updated web.js Hello World 3 again

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
-#!/usr/bin/env node
-var express = require("express");
-var app = express();
+var express = require('express');
 
-var fs = require('fs');
-
-var data = fs.readFileSync('index.html');
-
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(data.toString('utf-8'));
+  response.send('Hello World3!');
 });
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
