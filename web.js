3162afdd1d58909f70a7215252ac230d3249a3a7
Ubuntu <sahirkeshwani@yahoo.com>
2013-07-13 07:54:02 +0000

    web.js with index.html; take 1

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-app.get('/', function(request, response) {
-  response.send('Hello World4!');
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var data = fs.readFileSync('index.html');
+
+app.get('/', function(request, response) {
+  response.send(data.toString('utf-8'));
+});
+
